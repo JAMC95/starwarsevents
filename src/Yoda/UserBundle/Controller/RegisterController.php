8080641aee348372907c16de7b11dd7d45e6e2ef
Jose Antonio Moral Castilla <joxe.bailen@gmail.com>
2016-08-16 11:57:05 +0200

    Funcionabilidad real del /register/

--- src/Yoda/UserBundle/Controller/RegisterController.php
+++ src/Yoda/UserBundle/Controller/RegisterController.php
@@ -10,6 +10,16 @@ namespace Yoda\UserBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\DependencyInjection\Compiler\RepeatedPass;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Yoda\UserBundle\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+
 
 class RegisterController extends Controller
 {
@@ -17,15 +27,43 @@ class RegisterController extends Controller
      * @Route("/register", name="register_form")
      * @Template()
      */
-   public function registerAction() {
+   public function registerAction(Request $request) {
     $form=$this->createFormBuilder()
-        ->add('username','Symfony\Component\Form\Extension\Core\Type\TextType')
-        ->add('email','Symfony\Component\Form\Extension\Core\Type\EmailType')
-        ->add('password','Symfony\Component\Form\Extension\Core\Type\RepeatedType',array(
-            'type'=> 'Symfony\Component\Form\Extension\Core\Type\PasswordType'
-        ))
+        ->add('username', TextType::class)
+        ->add('email', EmailType::class)
+        ->add('password',RepeatedType::class,array('type'=>PasswordType::class))
+        ->add('save', SubmitType::class,array('label'=>'Registrarse'))
         ->getForm();
+
+
+       $form->handleRequest($request);
+       if($form->isValid()){
+       $data = $form->getData();
+
+           $user = new User();
+           $user->setUsername($data['username']);
+           $user->setEmail($data['email']);
+           $user->setPassword($this->encodePassword($user,$data['password']));
+
+           $em = $this->getDoctrine()->getManager();
+           $em->persist($user);
+           $em->flush();
+
+
+
+           return $this->redirectToRoute('event_index');
+       }
+
        return array('form'=>$form->createView());
    }
+
+    private function encodePassword(User $user, $plainPassword)
+    {
+        $encoder = $this->container->get('security.encoder_factory')
+            ->getEncoder($user)
+        ;
+
+        return $encoder->encodePassword($plainPassword, $user->getSalt());
+    }
 
 }
